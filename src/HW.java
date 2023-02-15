d5008a68f0716ba40397fe25c340ed5bef670652
hunter.weppler <hunter.weppler@my.simpson.edu>
2023-02-15 09:26:08 -0600

    1

--- src/HW.java
+++ src/HW.java
@@ -1,9 +1,18 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class HW {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Input a String ");
+public class HW{
+    public static int[] rotate(int[] numbers) {
+        int lstIndex = numbers[numbers.length - 1];
+    for (int index = numbers.length-2; index >= 0; index --) {
+            numbers[index + 1] = numbers[index];
+        }
+    numbers[0] = lstIndex;
+    return numbers;
+}
 
+    public static void main(String[] args){
+        int[] test = {1,2,3,4,5,6,7,8};
+        System.out.println(Arrays.toString(rotate(test)));
     }
 }
